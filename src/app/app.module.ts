ac792f40ad8475ad5411020827e56061034550b8
leolileo <l.li@campus.tu-berlin.de>
2018-08-08 04:46:08 +0200

    starting page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,45 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
-import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import {AppComponent} from './app.component';
+import {ContactDetailsComponent} from './contacts/contact-details/contact-details.component';
+import {ContactListComponent} from './contacts/contact-list/contact-list.component';
+import {RouterModule, Routes} from '@angular/router';
+import {NotfoundComponent} from './notfound/notfound.component';
+import {LandingpageComponent} from './landingpage/landingpage.component';
+
+const appRoutes: Routes = [
+  {path: 'crisis-center', component: NotfoundComponent},
+  {path: 'hero/:id', component: NotfoundComponent},
+  {
+    path: '',
+    component: LandingpageComponent,
+    data: {title: 'Heroes List'}
+  },
+/*  {
+    path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  },*/
+  {path: '**', component: NotfoundComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactDetailsComponent,
-    ContactListComponent
+    ContactListComponent,
+    NotfoundComponent,
+    LandingpageComponent,
+    LandingpageComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true} // <-- debugging purposes only
+    ),
     BrowserModule,
     FormsModule,
     HttpModule
@@ -21,4 +47,5 @@ import { ContactListComponent } from './contacts/contact-list/contact-list.compo
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
